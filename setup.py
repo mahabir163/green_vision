aecc2d3c2c30a66fa4033a65e1a0ae9906e26641
mahabirp163@gmail.com <mahabirp163@gmail.com>
2025-08-14 23:38:56 +0530

    This is my first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Green_Vision",
+    version="0.0.1",
+    author="Mahabir",
+    author_email="mahabirp163@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
+    
